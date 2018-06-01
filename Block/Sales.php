7a8684fd84b49096eab46969d301ef69b7c77d1c
Floriaan van Denderen <floriaan@tickles.nl>
2018-06-01 16:02:16 +0200

    Added Price formatting

--- Block/Sales.php
+++ Block/Sales.php
@@ -5,6 +5,7 @@ namespace Tickles\Supersales\Block;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Block\Product\ImageBuilder;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Pricing\Helper\Data;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Psr\Log\LoggerInterface;
@@ -26,24 +27,37 @@ class Sales extends Template
      * @var ImageBuilder
      */
     private $imageBuilder;
+    /**
+     * @var array
+     */
+    private $priceHelper;
 
     /**
      * Sales constructor.
      *
-     * @param Context                    $context
-     * @param SaleFactory                $saleFactory
-     * @param ProductRepositoryInterface $productRepository
-     * @param LoggerInterface            $logger
-     * @param ImageBuilder               $imageBuilder
-     * @param array                      $data
+     * @param Context                                $context
+     * @param SaleFactory                            $saleFactory
+     * @param ProductRepositoryInterface             $productRepository
+     * @param LoggerInterface                        $logger
+     * @param ImageBuilder                           $imageBuilder
+     * @param Data $priceHelper
+     * @param array                                  $data
      */
-    public function __construct(Context $context, SaleFactory $saleFactory, ProductRepositoryInterface $productRepository, LoggerInterface $logger, ImageBuilder $imageBuilder, array $data = [])
-    {
+    public function __construct(
+        Context $context,
+        SaleFactory $saleFactory,
+        ProductRepositoryInterface $productRepository,
+        LoggerInterface $logger,
+        ImageBuilder $imageBuilder,
+        Data $priceHelper,
+        array $data = []
+    ) {
         parent::__construct($context, $data);
         $this->saleFactory = $saleFactory;
         $this->productRepository = $productRepository;
         $this->logger = $logger;
         $this->imageBuilder = $imageBuilder;
+        $this->priceHelper = $priceHelper;
     }
 
     public function getImage($product, $imageId)
@@ -53,7 +67,14 @@ class Sales extends Template
                                   ->create();
     }
 
+    public function getFormattedPrice($price, $format = true, $includeContainer = true)
+    {
+        return $this->priceHelper->currency($price, $format, $includeContainer);
+    }
 
+    /**
+     * @return array
+     */
     public function getActiveSalesProducts() {
         // TODO - CHECK CURRENT DATETIME AGAINST START/END DATES OF THE SALE IN ADDITION TO IS_ENABLED
 
